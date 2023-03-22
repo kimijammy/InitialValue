0ecc0810342425bcbc6780c74b5cb2b9a5e5b0b2
kimijammy <125917969+kimijammy@users.noreply.github.com>
2023-03-22 12:43:55 +0000

    Update Initial.java

--- src/Initial.java
+++ src/Initial.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Initial {
+ublic class Initial {
+    int x;
+
+    public Initial(){
+        x = 5;
+    }
+
+    public static void main(String[] args) {
+        Initial myObj = new Initial();
+
+        System.out.println(myObj.x);
+    }
 }
